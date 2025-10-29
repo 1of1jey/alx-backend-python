3d7a3eb43ec2a23145d3077efdc6b83071947eea
jeffrey <eshunjeffrey12@gmail.com>
2025-10-29 06:38:31 +0000

    user data limit

--- python-generators-0x00/2-lazy_paginate.py
+++ python-generators-0x00/2-lazy_paginate.py
@@ -8,7 +8,7 @@ def paginate_users(page_size, offset):
         return []
     cursor = connection.cursor(dictionary=True)
     try:
-        cursor.execute(f"SELECT user_id, name, email, age FROM user_data LIMIT {page_size} OFFSET {offset}")
+        cursor.execute(f"SELECT * FROM user_data LIMIT {page_size} OFFSET {offset}")
         rows = cursor.fetchall()
         return rows
     finally:
