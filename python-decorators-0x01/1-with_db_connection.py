2f661c466be6cc61a8336c0c5e12e029a406a9f7
jeffrey <eshunjeffrey12@gmail.com>
2025-11-05 21:43:19 +0000

    Implement with_db_connection decorator
    Add a decorator to manage database connections.

--- python-decorators-0x01/1-with_db_connection.py
+++ python-decorators-0x01/1-with_db_connection.py
@@ -1,2 +1,15 @@
 import sqlite3 
 import functools
+
+def with_db_connection(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        # Open database connection
+        conn = sqlite3.connect('users.db')
+        try:
+            result = func(conn, *args, **kwargs)
+            return result
+        finally:
+            conn.close()
+    
+    return wrapper
