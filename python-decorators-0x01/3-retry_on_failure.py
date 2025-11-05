9e6721eace8c651e63f8af65b09d4e06bf439cbe
jeffrey <eshunjeffrey12@gmail.com>
2025-11-05 21:49:29 +0000

    Create 3-retry_on_failure.py

---
+++ python-decorators-0x01/3-retry_on_failure.py
@@ -0,0 +1,15 @@
+import time
+import sqlite3 
+import functools
+
+def with_db_connection(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        conn = sqlite3.connect('users.db')
+        try:
+            result = func(conn, *args, **kwargs)
+            return result
+        finally:
+            conn.close()
+    
+    return wrapper
