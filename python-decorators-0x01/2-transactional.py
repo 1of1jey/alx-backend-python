c2970a5f03fce32c00830162710c0263b684f9b5
jeffrey <eshunjeffrey12@gmail.com>
2025-11-05 21:47:00 +0000

    Update 2-transactional.py

--- python-decorators-0x01/2-transactional.py
+++ python-decorators-0x01/2-transactional.py
@@ -12,3 +12,25 @@ def with_db_connection(func):
             conn.close()
     
     return wrapper
+
+
+def transactional(func):
+    @functools.wraps(func)
+    def wrapper(conn, *args, **kwargs):
+        try:
+            result = func(conn, *args, **kwargs)
+            conn.commit()
+            return result
+        except Exception as e:
+            conn.rollback()
+            raise  # Re-raise the exception after rollback
+    
+    return wrapper
+
+@with_db_connection 
+@transactional 
+def update_user_email(conn, user_id, new_email): 
+    cursor = conn.cursor() 
+    cursor.execute("UPDATE users SET email = ? WHERE id = ?", (new_email, user_id))
+ 
+update_user_email(user_id=1, new_email='Crawford_Cartwright@hotmail.com')
