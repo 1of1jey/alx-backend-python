5118b3026d6b01aa1d75a8ef62217fab3728acab
jeffrey <eshunjeffrey12@gmail.com>
2025-11-05 21:45:56 +0000

    Update 2-transactional.py

--- python-decorators-0x01/2-transactional.py
+++ python-decorators-0x01/2-transactional.py
@@ -1,1 +1,14 @@
+import sqlite3 
+import functools
 
+def with_db_connection(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        conn = sqlite3.connect('users.db')
+        try:
+            result = func(conn, *args, **kwargs)
+            return result
+        finally:
+            conn.close()
+    
+    return wrapper
