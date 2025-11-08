20f2a4104129310ea9c2cec669bb5de0dfb0f1f4
jeffrey <eshunjeffrey12@gmail.com>
2025-11-08 21:15:49 +0000

    Update 3-concurrent.py

--- python-context-async-perations-0x02/3-concurrent.py
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -1,1 +1,10 @@
+import asyncio
+import aiosqlite
 
+async def async_fetch_users():
+    async with aiosqlite.connect('users.db') as db:
+        async with db.execute("SELECT * FROM users") as cursor:
+            results = await cursor.fetchall()
+            print("All users fetched:")
+            print(results)
+            return results
