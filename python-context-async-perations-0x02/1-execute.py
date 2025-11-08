f42e6a43f2becf1ca1dee0c5ae3e0aca2ac43640
jeffrey <eshunjeffrey12@gmail.com>
2025-11-08 21:13:05 +0000

    Update 1-execute.py

--- python-context-async-perations-0x02/1-execute.py
+++ python-context-async-perations-0x02/1-execute.py
@@ -1,1 +1,10 @@
+import sqlite3
 
+class ExecuteQuery:
+    def __init__(self, database, query, params=None):
+        self.database = database
+        self.query = query
+        self.params = params if params is not None else ()
+        self.connection = None
+        self.cursor = None
+        self.results = None
