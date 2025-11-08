10d0989cd82d8fb3a2571cb3495026c1aef652d5
jeffrey <eshunjeffrey12@gmail.com>
2025-11-08 21:10:23 +0000

    Update 0-databaseconnection.py

--- python-context-async-perations-0x02/0-databaseconnection.py
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -4,3 +4,12 @@ class DatabaseConnection:
     def __init__(self, database):
         self.database = database
         self.connection = None
+
+  def __enter__(self):
+        self.connection = sqlite3.connect(self.database)
+        return self.connection
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.connection:
+            self.connection.close()
+        return False
