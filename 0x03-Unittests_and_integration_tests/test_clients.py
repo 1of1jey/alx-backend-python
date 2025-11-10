a543739263658f486c15fb942a176e5d1c6f888c
jeffrey <eshunjeffrey12@gmail.com>
2025-11-10 11:47:49 +0000

    Update test_clients.py

--- 0x03-Unittests_and_integration_tests/test_clients.py
+++ 0x03-Unittests_and_integration_tests/test_clients.py
@@ -1,1 +1,9 @@
+#!/usr/bin/env python3
+"""
+Unit tests for GithubOrgClient.org method.
+"""
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
 
+from client import GithubOrgClient
