7d5be121e97a7c5fc05560f1d0a3595c262e6013
jeffrey <eshunjeffrey12@gmail.com>
2025-11-10 13:35:17 +0000

    Update fixtures.py

--- 0x03-Unittests_and_integration_tests/fixtures.py
+++ 0x03-Unittests_and_integration_tests/fixtures.py
@@ -1,1 +1,15 @@
+#!/usr/bin/env python3
+org_payload = {
+    "login": "google",
+    "id": 1,
+    "repos_url": "https://api.github.com/orgs/google/repos"
+}
+
+repos_payload = [
+    {"id": 1, "name": "truth", "license": {"key": "apache-2.0"}},
+    {"id": 2, "name": "autopilot", "license": {"key": "mit"}},
+]
+
+expected_repos = ["truth", "autopilot"]
+apache2_repos = ["truth"]
 
