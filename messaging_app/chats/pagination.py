cf23ec62c82201cb1a7ba842c6d679de10f81640
jeffrey <eshunjeffrey12@gmail.com>
2025-11-24 11:35:57 +0000

    fix

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -4,3 +4,12 @@ class MessagePagination(PageNumberPagination):
     page_size = 20
     page_size_query_param = 'page_size'
     max_page_size = 100
+
+    def get_paginated_response(self, data):
+        from rest_framework.response import Response
+        return Response({
+            'count': self.page.paginator.count,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data
+        })
